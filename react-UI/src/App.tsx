16bdc3bdec2062b205200985260532c9062af96e
ashish-2210-ashish <ashishsuresh2210@gmail.com>
2025-04-18 11:46:30 +0530

    adding cors to users controller and finishing working template for register user

--- react-UI/src/App.tsx
+++ react-UI/src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React ,{ Component } from "react"
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type RegisterState={
+  username :string;
+  password :string;
+  role :string;
 }
+
+
+class App extends Component<{},RegisterState>{
+
+constructor(){
+  super();
+  this.state={
+    username:"",
+    password:"",
+    role:"RIDER"
+  }
+
+}
+
+handelsubmit=async(event : React.FormEvent)=>{
+  event.preventDefault();
+  try{
+    await axios.post("http://localhost:8080/user/register",this.state);
+    alert("Registered successfully ...");
+  }
+  catch(e){
+    alert("Registered failed ...")
+    console.error(e);
+  }
+}
+
+handelchange=(event:any)=>{
+  this.setState({[event.target.name] : event.target.value})
+
+}
+
+render(){
+  return(
+    <form onSubmit={this.handelsubmit}>
+      <h1>Register</h1>
+      <input name="username"
+      placeholder="enter username"
+      type="email"
+      value={this.state.username}
+      onChange={this.handelchange}
+      />
+
+     <input name="password"
+      placeholder="enter password"
+      type="password"
+      value={this.state.password}
+      onChange={this.handelchange}
+      />
+
+    <select name="role" value={this.state.role} onChange={this.handleChange}>
+          <option value="RIDER">RIDER</option>
+          <option value="ADMIN">ADMIN</option>
+        </select>
+
+        <button type="submit">Register</button>
+     
+     
+    </form>
+    
+  )}
+}
+
 
 export default App
