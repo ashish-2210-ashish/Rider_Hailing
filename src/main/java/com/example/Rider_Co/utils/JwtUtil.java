e4c5960afc25af33ea11463270a086509fc8537a
ashish-2210-ashish <ashishsuresh2210@gmail.com>
2025-05-20 10:26:16 +0530

    refactoring the expiration time of the jwt token to 1 day from 30 minutes

--- src/main/java/com/example/Rider_Co/utils/JwtUtil.java
+++ src/main/java/com/example/Rider_Co/utils/JwtUtil.java
@@ -24,7 +24,7 @@ public class JwtUtil {
                 .setSubject(username)
                 .claim("role",role)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
