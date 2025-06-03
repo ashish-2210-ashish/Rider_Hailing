a02a49e4a9cdac878aa2c5200096f4caaf101853
ashish-2210-ashish <ashishsuresh2210@gmail.com>
2025-06-03 10:53:34 +0530

    creating the navigation bar template for admin home

--- react-UI/src/App.tsx
+++ react-UI/src/App.tsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
 import Register from "./components/RegisterComponent/Register";
-import Home from "./components/HomeComponent/Home";
+import Home from "./components/HomeComponent/AdminHome/Home";
 import Login from "./components/LoginComponent/Login";
-import DriverHome from "./components/HomeComponent/DriverHome/DriverHome";
-import RiderHome from "./components/HomeComponent/RiderHome/RiderHome";
+import AdminDriver from "./components/HomeComponent/AdminHome/AdminDriver";
+import AdminRider from "./components/HomeComponent/AdminHome/AdminRider";
 import { BrowserRouter as Router,Route ,Routes , Navigate} from "react-router-dom";
 import './App.scss'
 
@@ -17,8 +17,8 @@ class App extends Component {
           <Route path="/login" element={< Login/>} />
           <Route path="/register" element={< Register/>} />
           <Route path="/home" element={< Home/>} />
-          <Route path="/driverHome" element={< DriverHome/>} />
-          <Route path="/riderHome" element={< RiderHome/>} />
+          <Route path="/admindriver" element={< AdminDriver/>} />
+          <Route path="/adminrider" element={< AdminRider/>} />
         </Routes>
       </div>
       </Router>
