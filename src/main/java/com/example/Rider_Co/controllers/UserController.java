5b5a6a6a9b473b21641e53576bfcfa77d0c24581
ashish-2210-ashish <ashishsuresh2210@gmail.com>
2025-06-04 11:38:00 +0530

    changing the cors metadata

--- src/main/java/com/example/Rider_Co/controllers/UserController.java
+++ src/main/java/com/example/Rider_Co/controllers/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @CrossOrigin(
-        origins = "http://localhost:5174",
+        origins = "http://localhost:5175",
         allowCredentials = "true"
 )
 
