f09b7fd84389c09c9df5c0d18da78bb7a9f5086d
ashish-2210-ashish <ashishsuresh2210@gmail.com>
2025-06-04 11:36:36 +0530

    changing the cors origin

--- src/main/java/com/example/Rider_Co/controllers/DriverController.java
+++ src/main/java/com/example/Rider_Co/controllers/DriverController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5175", allowCredentials = "true")
+
 @RestController
 @RequestMapping("/driver")
 public class DriverController {
