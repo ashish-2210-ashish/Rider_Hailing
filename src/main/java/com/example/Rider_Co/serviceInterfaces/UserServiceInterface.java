4aafd1df8313449aa0e2aea46f0f015afd7a6124
ashish-2210-ashish <ashishsuresh2210@gmail.com>
2025-06-09 11:48:19 +0530

    creating a interface to get all the users

--- src/main/java/com/example/Rider_Co/serviceInterfaces/UserServiceInterface.java
+++ src/main/java/com/example/Rider_Co/serviceInterfaces/UserServiceInterface.java
@@ -2,9 +2,11 @@ package com.example.Rider_Co.serviceInterfaces;
 
 import com.example.Rider_Co.models.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserServiceInterface {
     String registerUser(User user);
     Optional<User> authenticate(String username, String rawPassword);
+    List<User> getAllUsers();
 }
